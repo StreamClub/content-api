56d01eb503658130b79b3bcbac15207b5201e1a7
Hugo <hlarrea@fi.uba.ar>
2024-02-03 17:49:20 -0300

    next episode name

--- src/entities/series/nextEpisode.ts
+++ src/entities/series/nextEpisode.ts
@@ -4,12 +4,14 @@ import { Episode } from "moviedb-promise";
 export class NextEpisode {
     photo: string;
     airDate: string;
+    name: string;
     episodeNumber: number;
     seasonNumber: number;
 
     constructor(episode: SeasonEpisode, seasonNumber: number) {
         this.photo = episode.poster;
         this.airDate = episode.airDate;
+        this.name = episode.name;
         this.episodeNumber = episode.episodeNumber;
         this.seasonNumber = seasonNumber;
     }
