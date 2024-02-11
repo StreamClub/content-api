9ae454d66a602b0cee834c72407c04397f2a98f6
Hugo Larrea <hlarrea@fi.uba.ar>
2024-02-11 20:10:48 -0300

    unwatch movie tests

--- tests/helpers/seenContentHelper.ts
+++ tests/helpers/seenContentHelper.ts
@@ -3,3 +3,7 @@ import { seenContentRepository } from "@dal"
 export const createSeenContentList = async (userId: number) => {
     await seenContentRepository.create(userId.toString());
 }
+
+export const seeMovie = async (userId: number, movieId: number) => {
+    await seenContentRepository.addMovie(userId.toString(), movieId);
+}
