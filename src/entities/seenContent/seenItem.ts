7da0cc3809d7a8d1b156aaa6ea4bb854a4c3fb06
Hugo Larrea <hlarrea@fi.uba.ar>
2024-02-18 13:48:49 -0300

    Add dummy fields

--- src/entities/seenContent/seenItem.ts
+++ src/entities/seenContent/seenItem.ts
@@ -2,10 +2,14 @@ export class SeenItem {
     public contentType;
     public updatedAt: Date;
     public id: number;
+    public hasReview: boolean;
+    public liked: boolean;
 
     public constructor(id: number, contentType: string, updatedAt: Date) {
         this.contentType = contentType;
         this.updatedAt = updatedAt;
         this.id = id;
+        this.hasReview = Math.random() >= 0.5; //  TODO: This is a placeholder, remove it
+        this.liked = Math.random() >= 0.5; //TODO: This is a placeholder, remove it
     }
 }
