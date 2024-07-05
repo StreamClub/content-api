77217f56ecd4c5bfc141cb12f21c574dab91a1e6
Hugo Larrea <hlarrea@fi.uba.ar>
2024-07-05 10:58:57 -0300

    fix in my platforms filter on discover

--- src/services/tmdbService.ts
+++ src/services/tmdbService.ts
@@ -201,7 +201,7 @@ export class TmdbService {
     ) {
         const platforms = inMyPlatforms ? (await this.streamProviderService.getAll(userId)).providerId : [];
         const result = await this.tmdb.discoverMovie({
-            language: this.language, page, watch_region: country, with_watch_providers: platforms.join(','),
+            language: this.language, page, watch_region: country, with_watch_providers: platforms.join('|'),
             with_genres: genderIds.join(','), "with_runtime.gte": runtimeGte, "with_runtime.lte": runtimeLte
         });
         const movies = await Promise.all(result.results.map(async (movie: MovieResult) => {
@@ -243,7 +243,7 @@ export class TmdbService {
     ) {
         const platforms = inMyPlatforms ? (await this.streamProviderService.getAll(userId)).providerId : [];
         const result = await this.tmdb.discoverTv({
-            language: this.language, page, watch_region: country, with_watch_providers: platforms.join(','),
+            language: this.language, page, watch_region: country, with_watch_providers: platforms.join('|'),
             with_genres: genderIds.join(','), "with_runtime.gte": runtimeGte, "with_runtime.lte": runtimeLte
         });
         const series = await Promise.all(result.results.map(async (serie: TvResult) => {
